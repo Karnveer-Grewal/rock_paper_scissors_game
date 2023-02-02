bfc3a1adc3ed5f3a40f2c274593a77201ca8c9b3
Karnveer Grewal <karnveergrewal4@gmail.com>
2023-02-02 12:50:21 -0500

    added result display

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 let userChoice;
 let computerChoice;
+let result;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    getResult();
   })
 );
 
@@ -27,4 +29,30 @@ function generateComputerChoice() {
   }
 
   computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "It's a Draw.";
+  }
+  if (computerChoice === 'rock' && userChoice === 'paper') {
+    result = 'You Win!!!';
+  }
+  if (computerChoice === 'rock' && userChoice === 'scissors') {
+    result = 'You Lost.';
+  }
+  if (computerChoice === 'paper' && userChoice === 'scissors') {
+    result = 'You Win!!!';
+  }
+  if (computerChoice === 'paper' && userChoice === 'rock') {
+    result = 'You Lost.';
+  }
+  if (computerChoice === 'scissors' && userChoice === 'rock') {
+    result = 'You Win!!!';
+  }
+  if (computerChoice === 'scissors' && userChoice === 'paper') {
+    result = 'You Lost.';
+  }
+
+  resultDisplay.innerHTML = result;
 }
